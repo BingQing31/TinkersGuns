af20be53f36f98cc0da5eef30730dfd260b90b96
霧雨烨 <Web_13234@outlook.com>
2026-03-28 23:38:48 +0800

    瞄准姿态初步完成，等待重构

--- src/main/java/com/kirisamey/tconguns/tools/animation/GunSmallBoneController.java
+++ src/main/java/com/kirisamey/tconguns/tools/animation/GunSmallBoneController.java
@@ -3,7 +3,11 @@ package com.kirisamey.tconguns.tools.animation;
 import com.kirisamey.toomanytinkers.models.AnimatableTicTool3DModelData;
 import com.kirisamey.toomanytinkers.models.pose.IAnimatableTicTool3DBoneController;
 import lombok.RequiredArgsConstructor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 
 @RequiredArgsConstructor
@@ -12,8 +16,22 @@ public class GunSmallBoneController implements IAnimatableTicTool3DBoneControlle
     private final IAnimatableTicTool3DBoneController controller;
 
     @Override
-    public AnimatableTicTool3DModelData.PosedBone pose(ItemStack itemStack, AnimatableTicTool3DModelData.BakedBone bakedBone, Matrix4f transform) {
-        transform.translate(0, 0, 0); // todo
-        return controller.pose(itemStack, bakedBone, transform);
+    public AnimatableTicTool3DModelData.PosedBone pose(ItemStack itemStack, AnimatableTicTool3DModelData.BakedBone bakedBone,
+                                                       @NotNull ItemDisplayContext itemDisplayContext, Matrix4f transform) {
+        var mc = Minecraft.getInstance();
+        if (itemDisplayContext.firstPerson() && mc.player != null) {
+            if (mc.player.isUsingItem()) {
+                // todo: 副手和双持的判定以后要考虑
+                if (mc.player.getUsedItemHand() == InteractionHand.MAIN_HAND && mc.player.getMainHandItem() == itemStack) {
+                    // todo: 这个坐标要放在 ItemStack 上，考虑好不同物品的差异和以后预定有的瞄具的调整。
+                    //       现在这个是手枪默认形态的
+                    //       这里除了这个偏移还有json模型上给的（后面的增减）
+                    //       另外不知道为啥这个y轴的坐标需要 /2 再用，但是后面json里的偏移不需要，也可能这里整个算错了也说不定但是真的搞不懂哇
+                    //       以及果然最好还是得把这个东西也放模型文件里面
+                    transform.translate(0.5125f + 0.125f, -0.19375f/2 - 0.140625f, 0f);
+                }
+            }
+        }
+        return controller.pose(itemStack, bakedBone, itemDisplayContext, transform);
     }
 }
