17eee6c0b595d528e7d372704eb86c05d4dcf7e8
霧雨烨 <Web_13234@outlook.com>
2026-04-16 03:47:59 +0800

    瞄准姿态数据化

--- src/main/java/com/kirisamey/tconguns/tools/animation/GunSmallBoneController.java
+++ src/main/java/com/kirisamey/tconguns/tools/animation/GunSmallBoneController.java
@@ -1,14 +1,21 @@
 package com.kirisamey.tconguns.tools.animation;
 
+import com.kirisamey.toomanytinkers.models.AnimatableTicTool3DFinalBakedModel;
 import com.kirisamey.toomanytinkers.models.AnimatableTicTool3DModelData;
 import com.kirisamey.toomanytinkers.models.pose.IAnimatableTicTool3DBoneController;
+import com.mojang.blaze3d.vertex.PoseStack;
 import lombok.RequiredArgsConstructor;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ItemTransform;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
+
+import java.util.HashMap;
 
 @RequiredArgsConstructor
 public class GunSmallBoneController implements IAnimatableTicTool3DBoneController {
@@ -16,22 +23,41 @@ public class GunSmallBoneController implements IAnimatableTicTool3DBoneControlle
     private final IAnimatableTicTool3DBoneController controller;
 
     @Override
-    public AnimatableTicTool3DModelData.PosedBone pose(ItemStack itemStack, AnimatableTicTool3DModelData.BakedBone bakedBone,
+    public AnimatableTicTool3DModelData.PosedBone pose(ItemStack itemStack, AnimatableTicTool3DFinalBakedModel model,
                                                        @NotNull ItemDisplayContext itemDisplayContext, Matrix4f transform) {
         var mc = Minecraft.getInstance();
-        if (itemDisplayContext.firstPerson() && mc.player != null) {
-            if (mc.player.isUsingItem()) {
+        var player = mc.player;
+        if (itemDisplayContext.firstPerson() && player != null) {
+            if (player.isUsingItem()) {
                 // todo: 副手和双持的判定以后要考虑
-                if (mc.player.getUsedItemHand() == InteractionHand.MAIN_HAND && mc.player.getMainHandItem() == itemStack) {
-                    // todo: 这个坐标要放在 ItemStack 上，考虑好不同物品的差异和以后预定有的瞄具的调整。
-                    //       现在这个是手枪默认形态的
-                    //       这里除了这个偏移还有json模型上给的（后面的增减）
-                    //       另外不知道为啥这个y轴的坐标需要 /2 再用，但是后面json里的偏移不需要，也可能这里整个算错了也说不定但是真的搞不懂哇
-                    //       以及果然最好还是得把这个东西也放模型文件里面
-                    transform.translate(0.5125f + 0.125f, -0.19375f/2 - 0.140625f, 0f);
+                if (player.getUsedItemHand() == InteractionHand.MAIN_HAND && player.getMainHandItem() == itemStack) {
+                    var modelTransform = model.getTransforms().getTransform(itemDisplayContext);
+                    var tr = reverseTranslation(modelTransform, false);
+                    transform.translate(tr);
+                    var t = model.getMarks().get("sight").getOrElse(new Vector3f());
+                    transform.translate(t.negate(new Vector3f()).mul(1f, 0.5f, 1f));
                 }
             }
         }
-        return controller.pose(itemStack, bakedBone, itemDisplayContext, transform);
+        return controller.pose(itemStack, model, itemDisplayContext, transform);
+    }
+
+
+    private final HashMap<ItemTransform, Vector3f> cacheR = new HashMap<>();
+    private final HashMap<ItemTransform, Vector3f> cacheL = new HashMap<>();
+
+    private Vector3f reverseTranslation(ItemTransform transform, boolean isLeftHand) {
+        var cache = isLeftHand ? cacheL : cacheR;
+        var value = cache.get(transform);
+
+        if (value == null) {
+            var poseStack = new PoseStack();
+            poseStack.pushPose();
+            transform.apply(isLeftHand, poseStack);
+            value = poseStack.last().pose().invert().transformPosition(new Vector3f());
+            cache.put(transform, value);
+        }
+
+        return value;
     }
 }
